8e80b0ae5f43f4faf13b8dc0ddded49ef6c3fd4c
Kathleen Francis Mbo <e1410158@cmaisonneuve.qc.ca>
2019-02-27 12:37:22 -0500

    animation opacité titre

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function(){
     console.log('oui animation');
+
+    let titre = document.querySelector('.site-title');
+    let sousTitre = document.querySelector('.site-description');
+
+    titre.classList.add("animTitre");
+    //sousTitre.classList.add("animSousTitre");
+
+    console.log(titre);
 })
